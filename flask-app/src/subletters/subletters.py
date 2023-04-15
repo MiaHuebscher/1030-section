3fbb4c781918eadd6c85485c3f39a72bb159d9da
Mia Huebscher <huebscher.m@northeastern.edu>
2023-04-15 16:09:05 -0400

    Update subletters.py

--- flask-app/src/subletters/subletters.py
+++ flask-app/src/subletters/subletters.py
@@ -8,7 +8,28 @@ subletters = Blueprint('subletters', __name__)
 # Add a sublettor profile to the database
 @subletters.route('/subletters', methods=['POST'])
 def create_subletter_profile():
-    pass
+    
+    # Access json data from request object
+    current_app.logger.info('Processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+
+    prod_name = req_data['product_name']
+    prod_description = req_data['product_description']
+    prod_price = req_data['product_listprice']
+    category = req_data['product_category']
+
+    # Construct the insert statement
+    insert_stmt = 'INSERT INTO products (product_name, description, category, list_price) VALUES ("'
+    insert_stmt += prod_name + '", "' + prod_description + '", "' + category + '", ' + str(prod_price) + ')'
+
+    current_app.logger.info(insert_stmt)
+
+    # Execute the query
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    return 'Success'
 
 # Delete a sublettor profile from the database
 @subletters.route('/subletters/<sblett_id>', methods=['DELETE'])
