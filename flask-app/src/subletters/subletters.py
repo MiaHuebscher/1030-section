c1f3c69b92fad2b3a48b4d28ddf9f0299e959407
patelkrina100 <130016349+patelkrina100@users.noreply.github.com>
2023-04-18 08:16:14 -0400

    Add files via upload

--- flask-app/src/subletters/subletters.py
+++ flask-app/src/subletters/subletters.py
@@ -35,10 +35,37 @@ def create_subletter_profile():
 # Delete a sublettor profile from the database
 @subletters.route('/subletters/<sblett_id>', methods=['DELETE'])
 def delete_subletter(sblett_id):
-    pass
+    subletter = Subletter.query.get(sblett_id)
+    if not subletter:
+        return jsonify({'error': 'Subletter not found'}), 404
+    
+    # Delete the subletter from the database
+    db.session.delete(subletter)
+    db.session.commit()
+
+    # Return a JSON response indicating success
+    return jsonify({'success': True})
+
     
 
 # Update a sublettor profile in the database
 @subletters.route('/subletters/<sblett_id>', methods=['PUT'])
 def update_subletter_profile(sblett_id):
-    pass
+
+    subletter = Subletter.query.get(sblett_id)
+    if not subletter:
+        return jsonify({'error': 'Subletter not found'}), 404
+    
+    # Update the subletter with the new information
+    subletter.sublett_id = request.json.get('sublett_id', subletter.sublett_id)
+    subletter.sblett_user = request.json.get('sblett_user', subletter.sblett_user)
+    subletter.sblett_age = request.json.get('sblett_age', subletter.sblett_age)
+    subletter.sblett_gender = request.json.get('sblett_gender', subletter.sblett_gender)
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    # Return a JSON response indicating success
+    return jsonify({'success': True})
+
+
