3a4298a21938d61d5757f42a58d8c9954cc53ce0
patelkrina100 <130016349+patelkrina100@users.noreply.github.com>
2023-04-18 21:38:04 -0400

    Add files via upload

--- flask-app/src/subletters/subletters.py
+++ flask-app/src/subletters/subletters.py
@@ -32,40 +32,78 @@ def create_subletter_profile():
 
     return 'Success'
 
-# Delete a sublettor profile from the database
+
+# Delete a sublettor profile in the database
 @subletters.route('/subletters/<sblett_id>', methods=['DELETE'])
 def delete_subletter(sblett_id):
-    subletter = Subletter.query.get(sblett_id)
-    if not subletter:
-        return jsonify({'error': 'Subletter not found'}), 404
-    
-    # Delete the subletter from the database
-    db.session.delete(subletter)
-    db.session.commit()
 
-    # Return a JSON response indicating success
-    return jsonify({'success': True})
+    # Access json data from request object
+    current_app.logger.info('Processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
 
-    
+    delete_stmt = 'DELETE FROM subletters WHERE sublett_id = ' + str(sblett_id)
+
+    current_app.logger.info(delete_stmt)
+
+    # Execute the query
+    cursor = db.get_db().cursor()
+    cursor.execute(delete_stmt)
+    db.get_db().commit()
+
+    return 'Success'
+
 
 # Update a sublettor profile in the database
 @subletters.route('/subletters/<sblett_id>', methods=['PUT'])
 def update_subletter_profile(sblett_id):
 
-    subletter = Subletter.query.get(sblett_id)
-    if not subletter:
-        return jsonify({'error': 'Subletter not found'}), 404
-    
-    # Update the subletter with the new information
-    subletter.sublett_id = request.json.get('sublett_id', subletter.sublett_id)
-    subletter.sblett_user = request.json.get('sblett_user', subletter.sblett_user)
-    subletter.sblett_age = request.json.get('sblett_age', subletter.sblett_age)
-    subletter.sblett_gender = request.json.get('sblett_gender', subletter.sblett_gender)
+    # Access json data from request object
+    current_app.logger.info('Processing form data')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
 
-    # Commit the changes to the database
-    db.session.commit()
+    sblett_user = req_data['sblett_user']
+    sblett_age = req_data['sblett_age']
+    sblett_gender = req_data['sblett_gender']
 
-    # Return a JSON response indicating success
-    return jsonify({'success': True})
+    if sblett_user != '': 
+        update_user_stmt = 'UPDATE subletters SET sblett_user = ' + '"' + sblett_user + '"' 
+        update_user_stmt += 'WHERE sublett_id =' + str(sblett_id)
+
+        current_app.logger.info(update_user_stmt)
+
+        # Execute the query
+        cursor = db.get_db().cursor()
+        cursor.execute(update_user_stmt)
+        db.get_db().commit()
+
+        return 'Success'
+
+    if sblett_age != '': 
+        update_age_stmt = 'UPDATE subletters SET sblett_age = ' + str(sblett_age)
+        update_age_stmt += 'WHERE sublett_id =' + str(sblett_id)
+
+        current_app.logger.info(update_age_stmt)
+
+        # Execute the query
+        cursor = db.get_db().cursor()
+        cursor.execute(update_age_stmt)
+        db.get_db().commit()
+
+        return 'Success'
+
+        
+    if sblett_gender != '': 
+        update_gender_stmt = 'UPDATE subletters SET sblett_gender = ' + '"' + sblett_gender + '"'
+        update_gender_stmt += 'WHERE sublett_id =' + str(sblett_id)
+
+        current_app.logger.info(update_stmt)
+
+        # Execute the query
+        cursor = db.get_db().cursor()
+        cursor.execute(update_gender_stmt)
+        db.get_db().commit()
 
 
+        return 'Success'
