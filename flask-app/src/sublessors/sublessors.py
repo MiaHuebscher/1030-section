6da5d3c5a44b7fe0d69df450ade9694e86d2ef34
Launna Atkinson <lsatk3@yahoo.com>
2023-04-20 12:36:07 -0400

    Comments

--- flask-app/src/sublessors/sublessors.py
+++ flask-app/src/sublessors/sublessors.py
@@ -87,6 +87,7 @@ def get_posts():
 
     return jsonify(json_data)
 
+
 # Get information about a specific posts
 @sublessors.route('/posts/<post_id>', methods=['GET'])
 def get_specific_post(post_id):
@@ -102,6 +103,7 @@ def get_specific_post(post_id):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
 
 # Get information about a posts under a specified price
 @sublessors.route('/posts/<unit_price>', methods=['GET'])
@@ -120,7 +122,7 @@ def get_inexpensive_posts(unit_price):
     return the_response
 
 
-# Update a sublessor profile in the database
+# Update a specific post content in the database
 @sublessors.route('/posts/post_dscrptn/<post_id>', methods=['PUT'])
 def update_post(post_id):
 
@@ -161,7 +163,7 @@ def delete_specific_post(post_id):
     return 'Success'
 
 
-# Delete a specific post that a sublessor wants to delete
+# Delete all posts created by a sublessor
 @sublessors.route('/posts/sublessor/<sbless_id>', methods=['DELETE'])
 def delete_posts_by_sbless(sbless_id):
 
@@ -176,12 +178,42 @@ def delete_posts_by_sbless(sbless_id):
 
     return 'Success'
 
-#jakob
-@sublessors.route('/reviews/review_content/<sbless_user>', methods=['GET']) #question about get
+
+# Create sublessor profile and add in details
+@sublessors.route('/sublessors', methods=['POST'])
+def create_sublessor_profile():
+
+   # Access json data from request object
+   current_app.logger.info('Processing form data')
+   req_data = request.get_json()
+   current_app.logger.info(req_data)
+
+   sbless_user = req_data['sbless_user2']
+   sbless_id = req_data['sbless_id2']
+   sbless_gender = req_data['sbless_gender']
+   sbless_age = req_data['sbless_age']
+
+   # Construct the insert statement
+   insert_stmt = 'INSERT INTO sublessors (sbless_user, sbless_id, sbless_gender, sbless_age) VALUES ("'
+   insert_stmt += sbless_user + '", ' + str(sbless_id) + ', "' + sbless_gender + '", ' + str(sbless_age) + ')'
+
+   current_app.logger.info(insert_stmt)
+
+   # Execute the query
+   cursor = db.get_db().cursor()
+   cursor.execute(insert_stmt)
+   db.get_db().commit()
+
+
+   return 'Success'
+
+
+# Get information about reviews on a specific sublessor
+@sublessors.route('/reviews/review_content/<sbless_user>', methods=['GET'])
 def get_reviews(sbless_user):
+
    # Get a cursor object from the database
    cursor = db.get_db().cursor()
-
 
    # Use cursor to query the database for a list of products
    cursor.execute('select rev_content from reviews \
@@ -190,14 +222,11 @@ def get_reviews(sbless_user):
    # Grab the column headers from the returned data
    row_headers = [x[0] for x in cursor.description]
 
-
    # Create an empty object to use in putting column headers together with data
    json_data = []
 
-
    # Fetch all the data from the cursor
    theData = cursor.fetchall()
-
 
    # For each of the rows, zip the data elements together with the column headers.
    for row in theData:
@@ -210,18 +239,44 @@ def get_reviews(sbless_user):
    return the_response
 
 
-@sublessors.route('/leases/<lease_id>', methods=['PUT'])
-def update_lease_profile(lease_id):
-
+# Create and add lease details about the sublet property
+@sublessors.route('/leases/<sbless_id>', methods=['POST'])
+def create_sublessor_lease(sbless_id):
 
    # Access json data from request object
    current_app.logger.info('Processing form data')
    req_data = request.get_json()
    current_app.logger.info(req_data)
 
+   lease_end_date = req_data['lease_end_date']
+   lord_id = req_data['lord_id']
+   lease_start_date = req_data['lease_start_date']
+   rent = req_data['rent']
+
+   # Construct the insert statement
+   insert_stmt = 'INSERT INTO leases (lease_end_date, lord_id, lease_start_date, rent, sbless_id) VALUES ("'
+   insert_stmt += lease_end_date + '", ' + str(lord_id) + ', "' + lease_start_date + '", ' + str(rent) + ', ' + str(sbless_id) + ')'
+
+   current_app.logger.info(insert_stmt)
+
+   # Execute the query
+   cursor = db.get_db().cursor()
+   cursor.execute(insert_stmt)
+   db.get_db().commit()
+
+   return 'Success'
+
+
+# Update lease information
+@sublessors.route('/leases/<lease_id>', methods=['PUT'])
+def update_lease_profile(lease_id):
+
+   # Access json data from request object
+   current_app.logger.info('Processing form data')
+   req_data = request.get_json()
+   current_app.logger.info(req_data)
 
    lease_end_date = req_data['lease_end_date2']
-
 
    update_lease_end_date = 'UPDATE leases SET lease_end_date = ' + '"' + lease_end_date + '"'
    update_lease_end_date += 'WHERE lease_id =' + str(lease_id)
@@ -234,71 +289,4 @@ def update_lease_profile(lease_id):
    db.get_db().commit()
 
    return 'Success'
-
-
-@sublessors.route('/leases/<sbless_id>', methods=['POST'])
-def create_sublessor_lease(sbless_id):
-
-
-   # Access json data from request object
-   current_app.logger.info('Processing form data')
-   req_data = request.get_json()
-   current_app.logger.info(req_data)
-
-
-   lease_end_date = req_data['lease_end_date']
-   lord_id = req_data['lord_id']
-   lease_start_date = req_data['lease_start_date']
-   rent = req_data['rent']
-
-
-   # Construct the insert statement
-   insert_stmt = 'INSERT INTO leases (lease_end_date, lord_id, lease_start_date, rent, sbless_id) VALUES ("'
-   insert_stmt += lease_end_date + '", ' + str(lord_id) + ', "' + lease_start_date + '", ' + str(rent) + ', ' + str(sbless_id) + ')'
-
-
-   current_app.logger.info(insert_stmt)
-
-
-   # Execute the query
-   cursor = db.get_db().cursor()
-   cursor.execute(insert_stmt)
-   db.get_db().commit()
-
-
-   return 'Success'
-
-
-@sublessors.route('/sublessors', methods=['POST'])
-def create_sublessor_profile():
-
-
-   # Access json data from request object
-   current_app.logger.info('Processing form data')
-   req_data = request.get_json()
-   current_app.logger.info(req_data)
-
-
-   sbless_user = req_data['sbless_user2']
-   sbless_id = req_data['sbless_id2']
-   sbless_gender = req_data['sbless_gender']
-   sbless_age = req_data['sbless_age']
-
-
-   # Construct the insert statement
-   insert_stmt = 'INSERT INTO sublessors (sbless_user, sbless_id, sbless_gender, sbless_age) VALUES ("'
-   insert_stmt += sbless_user + '", ' + str(sbless_id) + ', "' + sbless_gender + '", ' + str(sbless_age) + ')'
-
-
-   current_app.logger.info(insert_stmt)
-
-
-   # Execute the query
-   cursor = db.get_db().cursor()
-   cursor.execute(insert_stmt)
-   db.get_db().commit()
-
-
-   return 'Success'
-
 
